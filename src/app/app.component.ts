d05050d6225ec57767dd18834e1fce6028e86286
rahulphoke <135600207+rahulphoke@users.noreply.github.com>
2023-06-13 16:38:03 +0530

    ss
    demo

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { categoryMock } from './mock/mock';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app';
+  constructor(){
+  
+  }
+}
